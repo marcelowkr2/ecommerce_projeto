b96935be81a32fdc501ece26a9aad1c19461e115
marcelowkr2 <marcelowkr@hotmail.com>
2023-05-08 19:26:25 -0300

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./src/routes/routes')
+
+require('dotenv').config()
+
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, console.log('Banco de dados conectado com sucesso😄🚀'))
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(3333, () => console.log('Servidor Conectado😁👍'))
+
+
+
